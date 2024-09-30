c321b0b1c99de09d2489e63c7714e84c40d7abd9
G00389482@atu.ie <G00389482@atu.ie>
2024-09-30 13:43:06 +0100

    open feign interface added

--- src/main/java/atu/ie/week3_openfeign/Week3OpenFeignApplication.java
+++ src/main/java/atu/ie/week3_openfeign/Week3OpenFeignApplication.java
@@ -2,7 +2,9 @@ package atu.ie.week3_openfeign;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class Week3OpenFeignApplication {
 
